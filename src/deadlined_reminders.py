280723425d249a3ee9af4e0c3d26d3cf94a5ae07
Pontus <pontus.wed@gmail.com>
2020-10-31 13:27:25 +0100

    Tests passing up to and including task 6

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,26 @@
+from abc import ABC, ABCMeta, abstractmethod
+from collections.abc import Iterable
+from dateutil.parser import parse
+from datetime import datetime
+
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DeadlinedReminder(ABC, Iterable):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
+    def is_due(self):
+        if self.date <= datetime.now(): 
+            return True
+        return False
